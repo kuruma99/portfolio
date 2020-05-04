610024482e190acfbae85811fb99580f6b7a4197
kuruma99 <msmridulsingh@gmail.com>
2020-05-04 13:51:40 +0530

    Updates

---
+++ precache-manifest.4d59b28bbb079e3b9234b28207ab4ae5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b18a3e0d5c95672c3fcd15ee99048e6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "79867eb1337d750f062b",
+    "url": "/portfolio/static/css/2.87fde1fd.chunk.css"
+  },
+  {
+    "revision": "971efca85ad4287d68b5",
+    "url": "/portfolio/static/css/main.222c097b.chunk.css"
+  },
+  {
+    "revision": "79867eb1337d750f062b",
+    "url": "/portfolio/static/js/2.60b25cad.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.60b25cad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "971efca85ad4287d68b5",
+    "url": "/portfolio/static/js/main.2ab71dda.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "09f5c6ae0d73877c4fc2595170735ff9",
+    "url": "/portfolio/static/media/github.09f5c6ae.png"
+  },
+  {
+    "revision": "efdd715732003da9b72dd2224f43623b",
+    "url": "/portfolio/static/media/myself.efdd7157.jpeg"
+  }
+]);
